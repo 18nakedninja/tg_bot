2d5b2584a42f35b9e69e1f4a556be61ad5e060cb
18nakedninja <108285622+18nakedninja@users.noreply.github.com>
2025-09-12 16:04:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telegram.ext import (
 )
 
 # === ЛОГИ ===
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # === НАСТРОЙКИ ===
